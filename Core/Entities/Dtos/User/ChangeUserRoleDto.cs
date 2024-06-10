f0c9f52891c1aa4688df900631b201ee2b7eba38
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-10 03:28:05 +0300

    Create dtos and system roles enum

---
+++ Core/Entities/Dtos/User/ChangeUserRoleDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Entities.Dtos.User;
+
+public class ChangeUserRoleDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
