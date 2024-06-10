92f3149296711a810fde247599b576ae66a8206a
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-10 03:24:02 +0300

    Add max length data annotation to string properties for performance

--- Core/Entities/Concretes/Role.cs
+++ Core/Entities/Concretes/Role.cs
@@ -1,8 +1,9 @@
-﻿using Core.Entities.Abstracts;
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities.Abstracts;
 
 namespace Core.Entities.Concretes;
 
 public sealed class Role : AbstractEntity
 {
-    public string Name { get; set; } = string.Empty;
+    [MaxLength(50)] public string Name { get; set; } = string.Empty;
 }
--- Core/Entities/Concretes/User.cs
+++ Core/Entities/Concretes/User.cs
@@ -1,15 +1,16 @@
-﻿using Core.Entities.Abstracts;
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities.Abstracts;
 
 namespace Core.Entities.Concretes;
 
 public sealed class User : AbstractEntity
 {
     public Guid RoleId { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    [MaxLength(50)] public string FirstName { get; set; } = string.Empty;
+    [MaxLength(75)] public string LastName { get; set; } = string.Empty;
+    [MaxLength(150)] public string Email { get; set; } = string.Empty;
     public bool IsEmailVerified { get; set; } = false;
-    public string Username { get; set; } = string.Empty;
+    [MaxLength(50)] public string Username { get; set; } = string.Empty;
 
     public byte[] PasswordHash { get; set; } = [];
     public byte[] PasswordSalt { get; set; } = [];
