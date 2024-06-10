d6aa65790c0b0d030fbe4268c1dd3f37efe90d74
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-10 04:47:37 +0300

    Remove mail settings properties setters

--- Core/Mailing/MailSettings.cs
+++ Core/Mailing/MailSettings.cs
@@ -2,10 +2,10 @@
 
 public sealed class MailSettings
 {
-    public string SmtpServer { get; set; } = "[SMTP_SERVER]";
-    public int SmtpPort { get; set; } = 587;
-    public string SmtpUserName { get; set; } = "[USERNAME]";
-    public string SmtpPassword { get; set; } = "[PASSWORD]";
-    public string DisplayName { get; set; } = "[DISPLAY_NAME]";
-    public string From { get; set; } = "[FROM_EMAIL]";
+    public string SmtpServer { get; } = "[SMTP_SERVER]";
+    public int SmtpPort { get; } = 587;
+    public string SmtpUserName { get; } = "[USERNAME]";
+    public string SmtpPassword { get; } = "[PASSWORD]";
+    public string DisplayName { get; } = "[DISPLAY_NAME]";
+    public string From { get; } = "[FROM_EMAIL]";
 }
