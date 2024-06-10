88bc70d1d21c3fa27ccde48c9ca00e1d5f4a9e1d
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-10 03:25:41 +0300

    Create EmailVerification entity

---
+++ Core/Entities/Concretes/EmailVerification.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Entities.Abstracts;
+
+namespace Core.Entities.Concretes;
+
+public sealed class EmailVerification : AbstractEntity
+{
+    [MaxLength(50)] public string Username { get; set; } = string.Empty;
+    [MaxLength(81)] public string Token { get; set; } = string.Empty;
+}
