b578c8b388d083ec9f899a596ad20de71ce4fd12
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-21 21:17:08 +0300

    Change AuthorizationErrorDetails.cs default message

--- Core/Exceptions/Details/AuthorizationErrorDetails.cs
+++ Core/Exceptions/Details/AuthorizationErrorDetails.cs
@@ -6,7 +6,7 @@ namespace Core.Exceptions.Details;
 public sealed class AuthorizationErrorDetails : IErrorDetails
 {
     public int StatusCode { get; } = StatusCodes.Status401Unauthorized;
-    public string Message { get; } = "Bu alana erişmek için yetkin yok. Bir sorun olduğunu düşünüyorsan bizimle iletişime geçebilirsin.";
+    public string Message { get; } = "Bu alana erişmek için yetkin yok. Bir sorun olduğunu düşünüyorsan bizimle iletişime geçebilir ya da yeniden giriş yapmayı deneyebilirsin.";
     public string GetDetails()
     {
         return JsonConvert.SerializeObject(this);
