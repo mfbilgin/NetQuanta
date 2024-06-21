13408a8a6e4e1b30c77fecb74a2aa20c430f536a
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-21 21:17:46 +0300

    Throw an InvalidOperationException when _cacheManager was null.

--- Core/Aspects/Autofac/Caching/CacheAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -12,7 +12,7 @@ public sealed class CacheAspect(int duration = 60) : MethodInterception
 
     public override void Intercept(IInvocation invocation)
     {
-        if (_cacheManager == null) return;
+        if (_cacheManager == null) throw new InvalidOperationException("ICacheManager service is not available.");
         var methodName = string.Format($"{invocation.Method.ReflectedType?.FullName}.{invocation.Method.Name}");
         var arguments = invocation.Arguments.ToList();
         var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
@@ -21,6 +21,7 @@ public sealed class CacheAspect(int duration = 60) : MethodInterception
             invocation.ReturnValue = _cacheManager.Get(key);
             return;
         }
+
         invocation.Proceed();
         _cacheManager.Add(key, invocation.ReturnValue, duration);
     }
