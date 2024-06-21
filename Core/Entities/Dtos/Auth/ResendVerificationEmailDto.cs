b9c58f5fd9265ae41f4b6b8eb9dff8d3be71096c
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-21 21:31:27 +0300

    Create ResendVerificationEmailDto

---
+++ Core/Entities/Dtos/Auth/ResendVerificationEmailDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Entities.Dtos.Auth;
+
+public sealed class ResendVerificationEmailDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
