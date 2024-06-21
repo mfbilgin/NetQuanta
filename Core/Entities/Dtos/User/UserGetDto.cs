594acb57b44227e43a378881e846f21cf131b7b8
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-21 21:30:37 +0300

    Remove forgotten StudentNumber field

--- Core/Entities/Dtos/User/UserGetDto.cs
+++ Core/Entities/Dtos/User/UserGetDto.cs
@@ -3,7 +3,6 @@
 public sealed class UserGetDto
 {
     public Guid Id { get; set; } = Guid.Empty;
-    public string? StudentNumber { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
