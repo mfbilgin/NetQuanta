807608d10378492c06214ba15f4012b55df4cef2
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-22 15:26:25 +0300

    Create LogMessages

---
+++ Business/Constants/Messages/LogMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Business.Constants.Messages;
+
+public static class LogMessages
+{
+    public const string LogNotFound = "Böyle bir log yok. Admin olarak bu hatayı yapmış olman beni üzdü :) Kodlara bi göz at ya da uslu dur.";
+    public const string LogHasBeenDeleted = "Log başarıyla silindi.";
+    public const string LogsHasBeenDeleted = "Loglar başarıyla silindi.";
+}
