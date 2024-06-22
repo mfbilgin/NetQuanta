415367bf066c058e94c86289f3f7947424c16db6
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-22 15:28:09 +0300

    Create PasswordResetToken

---
+++ Core/Entities/Concretes/PasswordResetToken.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities.Abstracts;
+
+namespace Core.Entities.Concretes;
+
+public class PasswordResetToken : AbstractEntity
+{
+    public string Token { get; set; } = string.Empty;
+}
