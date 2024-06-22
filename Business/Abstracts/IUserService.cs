c641f8d66fef751eb1bd92859e1f309ad4ab8114
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-22 15:25:46 +0300

    Change method ordering

--- Business/Abstracts/IUserService.cs
+++ Business/Abstracts/IUserService.cs
@@ -9,10 +9,10 @@ public interface IUserService
     public void Add(User user);
     public void Update(UserUpdateDto userUpdateDto);
     public void Delete(UserDeleteDto userDeleteDto);
-    public void ChangePassword(ChangePasswordDto changePasswordDto);
-    public void ChangeUserRole(ChangeUserRoleDto changeUserRoleDto);
-    public void VerifyEmail(string username);
     public PageableModel<UserGetDto> GetAll(int index = 1, int size = 10);
     public UserGetDto? GetById(Guid id);
     public UserGetDto? GetByUsername(string name);
+    public void VerifyEmail(string username);
+    public void ChangeUserRole(ChangeUserRoleDto changeUserRoleDto);
+    public void ChangePassword(ChangePasswordDto changePasswordDto);
 }
