a7a47a9d09cb5d0be5e833e1eb39881dc30c94b7
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-24 16:54:39 +0300

    Create ResetPasswordDto.cs

---
+++ Core/Entities/Dtos/Auth/ResetPasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Dtos.Auth;
+
+public class ResetPasswordDto
+{
+    public string Token { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
