153382959a22db5acbafbdf35799d10bf85be3c3
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-24 17:07:11 +0300

    Add initial value

--- Core/Entities/Dtos/User/ChangePasswordDto.cs
+++ Core/Entities/Dtos/User/ChangePasswordDto.cs
@@ -2,7 +2,7 @@
 
 public sealed class ChangePasswordDto
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.Empty;
     public string CurrentPassword { get; set; } = string.Empty;
     public string NewPassword { get; set; } = string.Empty;
 }
