18b8370f24f14ca3ce457a830c6d6129a646da9e
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-24 17:06:10 +0300

    Change username field to requested value

--- DataAccess/Abstracts/ILogRepository.cs
+++ DataAccess/Abstracts/ILogRepository.cs
@@ -9,7 +9,7 @@ public interface ILogRepository : IEntityRepository<Log>
 {
     public void DeleteAllLogs();
     public void DeleteLogRange(List<Guid> logIds);
-    public PageableModel<Log> GetLogsByUsername(string username, int index = 1, int size = 10);
+    public PageableModel<Log> GetLogsByRequestedValue(string requestedValue, int index = 1, int size = 10);
     public PageableModel<Log> GetLogsByLogLevel(string logLevel, int index = 1, int size = 10);
     public PageableModel<Log> GetLogsByException(string exception, int index = 1, int size = 10);
 }
