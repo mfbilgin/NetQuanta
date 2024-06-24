87751080c4d4c43abffa4de3b6306ce09e056e0c
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-24 17:05:33 +0300

    Add custom mail send methods

--- Core/Mailing/IMailService.cs
+++ Core/Mailing/IMailService.cs
@@ -1,9 +1,9 @@
-﻿using MimeKit;
-
-namespace Core.Mailing;
+﻿namespace Core.Mailing;
 
 public interface IMailService
 {
-    protected void SendMail(string toEmail, string toUsername, string subject, TextPart body);
     public void SendWelcomeMail(string email,string username,string token);
+    public void SendPasswordResetMail(string email,string username,string token);
+    public void SendPasswordChangedMail(string email, string username);
+    public void SendUsernameReminderMail(string email, string username);
 }
